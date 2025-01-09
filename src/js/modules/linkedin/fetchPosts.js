ee05ac2ea8a06fbac0c584050201b3737922fa97
Studiopresto <hello@studiopresto.com>
2025-01-09 10:53:52 +0200

    update: changed Hero block for Home, added Linkedin posts

--- src/js/modules/linkedin/fetchPosts.js
+++ src/js/modules/linkedin/fetchPosts.js
@@ -13,21 +13,20 @@ export const fetchLinkedInPosts = async () => {
 	// 	.then(data => console.log(data))
 	// 	.catch(error => console.error('Error:', error));
 	
-	const data = {
-		grant_type: 'client_credentials',
-		client_id: '78txmoo7wraw85',
-		client_secret: 'WPL_AP1.3r0ePjSoabgSHmO2.Ipn45Q==',
-	};
-	
-	await fetch('https://www.linkedin.com/oauth/v2/accessToken', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: new URLSearchParams(data),
-	})
-		.then(response => response.json())
-		.then(data => console.log('Access Token:', data.access_token))
-		.catch(error => console.error('Error:', error));
-	
+	// const data = {
+	// 	grant_type: 'client_credentials',
+	// 	client_id: '78txmoo7wraw85',
+	// 	client_secret: 'WPL_AP1.3r0ePjSoabgSHmO2.Ipn45Q==',
+	// };
+	//
+	// await fetch('https://www.linkedin.com/oauth/v2/accessToken', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/x-www-form-urlencoded',
+	// 	},
+	// 	body: new URLSearchParams(data),
+	// })
+	// 	.then(response => response.json())
+	// 	.then(data => console.log('Access Token:', data.access_token))
+	// 	.catch(error => console.error('Error:', error));
 }
